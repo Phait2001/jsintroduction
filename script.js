0a0bcd886e273e1a666c918d8b41e02e464657c4
Daniel Faith <104217410+Phait2001@users.noreply.github.com>
2022-06-24 16:18:02 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+let name="Faith Daniel";
+let height="5ft 8in"
+let country="Nigeria"
+
+
+document.write(`<h1>Hello! \n</h1>`)
+document.write(`<h2> My name is ${name}</h2>`)
+document.write(`<h2> I am ${height} tall</h2>`)
+document.write(`<h2>I am from ${country}</h2>`)
